96118aa8b53407dfdbe3e0b8bcce81f388f13a48
Larissa Marães <larissa.maraes@jera.com.br>
2019-05-28 20:12:39 -0400

    creating adapter and view holder for pokemons list

---
+++ app/src/main/java/com/example/pokemonapp/PokemonAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.pokemonapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PokemonAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
+
+    var pokemons: List<String> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        return PokemonViewHolder.inflate(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return pokemons.size
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        holder.setItem(pokemons[position])
+    }
+
+    internal fun observePokemons(pokemons: List<String>) {
+        this.pokemons = pokemons
+    }
+}
---
+++ app/src/main/java/com/example/pokemonapp/PokemonViewHolder.kt
@@ -0,0 +1,21 @@
+package com.example.pokemonapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokemonapp.databinding.ItemPokemonBinding
+
+class PokemonViewHolder private constructor(
+    private val binding: ItemPokemonBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    internal fun setItem(pokemon: String) {
+        binding.textPokemon.text = pokemon
+    }
+
+    companion object {
+        fun inflate(parent: ViewGroup) : PokemonViewHolder {
+            return  PokemonViewHolder(ItemPokemonBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
+    }
+}
