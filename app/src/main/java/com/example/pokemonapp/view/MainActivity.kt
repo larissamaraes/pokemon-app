f8b7836decbe546800e9811d5d43ec5b7dc816b3
Larissa Marães <larissa.maraes@jera.com.br>
2019-05-30 19:32:02 -0400

    requesting pokemon page list

--- app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
@@ -1,59 +1,75 @@
 package com.example.pokemonapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.paging.PagedList
+import androidx.paging.RxPagedListBuilder
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokemonapp.R
 import com.example.pokemonapp.databinding.ActivityMainBinding
-import com.example.pokemonapp.model.ApiResponse
-import com.example.pokemonapp.remote.ApiService
+import com.example.pokemonapp.model.Pokemon
 import com.example.pokemonapp.remote.NetworkUtils
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.pokemonapp.remote.paging.PokemonDataSourceFactory
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var pokemonAdapter: PokemonAdapter
 
+    private val compositeDisposable = CompositeDisposable()
+    private val sourceFactory: PokemonDataSourceFactory
+
+    private var pokemonList: Observable<PagedList<Pokemon>>
+
+    init {
+        sourceFactory = PokemonDataSourceFactory(compositeDisposable, NetworkUtils.getRetrofitInstance())
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(NetworkUtils.PAGE_SIZE)
+            .setInitialLoadSizeHint(NetworkUtils.PAGE_SIZE)
+            .setPrefetchDistance(10)
+            .setEnablePlaceholders(false)
+            .build()
+
+        pokemonList = RxPagedListBuilder(sourceFactory, config)
+            .setFetchScheduler(Schedulers.io())
+            .buildObservable()
+            .cache()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupAdapter()
-        getPokemons()
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
     }
 
     private fun setupAdapter() {
         pokemonAdapter = PokemonAdapter()
-        with(binding.recyclerViewPokemons){
+        with(binding.recyclerViewPokemons) {
             adapter = pokemonAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
     }
 
-    private fun getPokemons() {
-        val retrofitClient = NetworkUtils.getRetrofitInstance()
-        val endpoint = retrofitClient.create(ApiService::class.java)
-        val callback = endpoint.getPokemons(0, 20)
-
-        callback.enqueue(object : Callback<ApiResponse> {
-            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "No Pokémons found :(", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                with(response.body()?.results ?: listOf()) {
-                    if (isEmpty()) {
-                        Toast.makeText(this@MainActivity, "No Pokémons found :(", Toast.LENGTH_LONG).show()
-                    } else {
-                        pokemonAdapter.observePokemons(this)
-                    }
-                }
-            }
-
-        })
+    private fun subscribeToList() {
+        compositeDisposable.add(pokemonList
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { list ->
+                    pokemonAdapter.submitList(list)
+                },
+                { /* Nothing to do here */ }
+            )
+        )
     }
 }
