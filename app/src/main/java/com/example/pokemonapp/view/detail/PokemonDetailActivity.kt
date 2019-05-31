bc4d9e19af6f71ba833e93ba197b40cfb83532ed
Larissa Marães <larissa.maraes@jera.com.br>
2019-05-30 21:01:08 -0400

    requestion pokemon details

--- app/src/main/java/com/example/pokemonapp/view/detail/PokemonDetailActivity.kt
+++ app/src/main/java/com/example/pokemonapp/view/detail/PokemonDetailActivity.kt
@@ -3,16 +3,23 @@ package com.example.pokemonapp.view.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.pokemonapp.R
 import com.example.pokemonapp.databinding.ActivityPokemonDetailBinding
+import com.example.pokemonapp.model.PokemonDetail
+import com.example.pokemonapp.remote.NetworkUtils
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class PokemonDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPokemonDetailBinding
 
-    private val pokemonNameExtra : String by lazy { intent.getStringExtra(POKEMON_NAME) }
+    private val pokemonNameExtra: String by lazy { intent.getStringExtra(POKEMON_NAME) }
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +27,31 @@ class PokemonDetailActivity : AppCompatActivity() {
         getPokemonDetail()
     }
 
-    private fun getPokemonDetail() {
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
 
+    private fun getPokemonDetail() {
+        compositeDisposable.add(
+            NetworkUtils.getRetrofitInstance().getPokemonDetail(pokemonNameExtra)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(::onSuccess, ::onFailure)
+        )
+    }
+
+    private fun onSuccess(detail: PokemonDetail) {
+        with(binding) {
+            textPokemonName.text = detail.name?.toUpperCase()
+            textBaseExperience.text = detail.baseExperience.toString() + " EXP"
+            textHeight.text = detail.height.toString() + " decimeters"
+            textWeight.text = detail.weight.toString() + " hectograms"
+        }
+    }
+
+    private fun onFailure(throwable: Throwable) {
+        Toast.makeText(this, "Something went wrong :(", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
